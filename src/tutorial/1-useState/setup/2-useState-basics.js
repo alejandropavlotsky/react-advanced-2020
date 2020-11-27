88e6ac88bd603e3cb20f58218465f5599dc5eae3
Alejandro Murawczik <amurawczik@almacorp.es>
2020-11-27 19:39:07 +0100

    ADD steps 1 to 4

--- src/tutorial/1-useState/setup/2-useState-basics.js
+++ src/tutorial/1-useState/setup/2-useState-basics.js
@@ -1,7 +1,19 @@
 import React, { useState } from 'react';
 
 const UseStateBasics = () => {
-  return <h2>useState basic example</h2>;
+  
+  const [text, setText] = useState('random title')
+
+  const handleClick = () => {
+    text === 'random title' ? setText('Hello world') : setText('random title')
+  }
+  
+  return (
+    <>
+      <h1>{text}</h1>
+      <button type="button" className="btn" onClick={handleClick}>Change title</button>
+    </>
+  )
 };
 
 export default UseStateBasics;
